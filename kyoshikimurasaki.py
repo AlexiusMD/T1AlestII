383b7c5632558b74e9e8c390df50fb648919f730
Alexius Dias <alexiusmd2004@gmail.com>
2024-03-08 15:23:04 -0300

    update main file/upload tests

--- kyoshikimurasaki.py
+++ kyoshikimurasaki.py
@@ -1,37 +1,52 @@
-file = open("map.txt", "r")
-caminho = []
-ySize = 0
-for x in file:
-    xSize = 0
-    for letter in x:
-        caminho.append(letter)
-        xSize += 1
-    ySize += 1
+def readMap(path):
+    caminho = []
+    file = open(path, "r")
+    next(file)
+    next(file)
+    for x in file:
+        if x != '':
+            for letter in x:
+                caminho.append(letter)
+                print(letter)
+    return caminho
 
+def readMapSize(path):
+    file = open(path, "r")
+    size = file.readline().split(" ")
+    return size
+
+def printMatrix(matrix):
+    for row in matrix:
+        print(row)
+
+path = "map.txt"
+
+caminho = readMap(path)
+ySize = int(readMapSize(path)[0])
+xSize = int(readMapSize(path)[1])
 matrix = []
-
-print("size of file is" + str(len(caminho)))
 
 for x in range(xSize): ##cria a fucking matrix
     row = []
-    linha = open("map.txt").readline()
     for y in range(ySize):
-        ##print("adding " + str(caminho[x + y * xSize]) + " to position " + str(x) + ", " + str(y))
-        row.append(linha[x])
+        #print("adding " + str(caminho[(x + y * ySize) - 1]) + " to position " + str(x) + ", " + str(y))
+        row.append(caminho[(x + y * xSize) - 2])
     matrix.append(row)
 
+    printMatrix(matrix)
+
 xPosition = 0
-yPosition = 0
+yPosition = getYPos(matrix)
 
 xVelocity = 1
 yVelocity = 0
 
 total = 0
 toAdd = ""
-while (yPosition <= ySize and xPosition <= xSize):
+while (yPosition < ySize and xPosition < xSize):
     currentChar = matrix[xPosition][yPosition]
 
-    print("Character in position " + str(xPosition) + ", " + str(yPosition) + " is " + currentChar)
+    #print("Character in position " + str(xPosition) + ", " + str(yPosition) + " is " + currentChar)
 
     if (currentChar.isdigit()):
         toAdd += currentChar
